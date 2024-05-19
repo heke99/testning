bf79696b8ba21eaa98b3e0ff5453cb2d8f17d5ff
heke99 <heke99@live.se>
2024-05-19 17:43:15 +0200

    First commit

---
+++ Backend/IsPas­sword­GoodE­nough.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+public class PasswordChecker
+{
+    public static bool IsPasswordGoodEnough(string password)
+    {
+        if (password.Length < 8)
+        {
+            return false;
+        }
+        if (!Regex.IsMatch(password, "[a-z]"))
+        {
+            return false;
+        }
+        if (!Regex.IsMatch(password, "[A-Z]"))
+        {
+            return false;
+        }
+        if (!Regex.IsMatch(password, "[0-9]"))
+        {
+            return false;
+        }
+        if (!Regex.IsMatch(password, "[^a-zA-Z0-9]"))
+        {
+            return false;
+        }
+        return true;
+    }
+
+    public static void Main()
+    {
+        
+}
---
+++ Backend/Remo­ve­Bad­Words.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+using System.Text.RegularExpressions;
+
+public class BadWords
+{
+    private static string[] badWords;
+
+    static BadWords()
+    {
+        string badWordsFilePath = Path.Combine("Backend", "json", "bad-words.json");
+
+        
+        {
+            // parsea json
+            string jsonContent = File.ReadAllText(badWordsFilePath);
+            badWords = JsonSerializer.Deserialize<string[]>(jsonContent);
+        }
+        
+    }
+
+    public static string RemoveBadWords(string text, string replacement)
+    {
+        foreach (string badWord in badWords)
+        {
+            string pattern = @"\b" + Regex.Escape(badWord) + @"\b";
+            text = Regex.Replace(text, pattern, replacement, RegexOptions.IgnoreCase);
+        }
+        return text;
+    }
+
+    public static void Main()
+    {
+       
+        {
+            string text = "This is a badWord.";
+            string replacement = "HejHej";
+
+            string cleanedText = RemoveBadWords(text, replacement);
+            Console.WriteLine(cleanedText);
+        }
+
+       
+    }
+}
